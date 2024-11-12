73ea5ef92cdb0e37451f5fc28a62ae630e3eef02
Achraf <hanzazachraf581@gmail.com>
2024-11-13 00:23:58 +0100

    huge change

--- main.js
+++ main.js
@@ -1,5 +1,33 @@
+let iconCart = document.querySelector('.icon-cart');
+let closeCart = document.querySelector('.close')
+let body = document.querySelector('body');
+let listProductHTML = document.querySelector('.listProduct');
 
+let listProducts = [];
 
+iconCart.addEventListener('click', () => {
+    console.log('Icon cliked')
+    body.classList.toggle('showCart')
+})
+closeCart.addEventListener('click', () => {
+    body.classList.toggle('showCart')
+})
+
+const addDataToHTML = () => {
+    listProductHTML.innerHTML = '';
+}
+
+const initApp = () => {
+    //get data from json
+    fetch('assets/js/data.json')
+    .then(response => response.json())
+    .then(data => {
+        listProducts = data;
+        addDataToHTML();
+    })
+}
+
+initApp();
 // script that will be in all pages
 // nav
 // cart
